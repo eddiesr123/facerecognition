825e62309407f7d411bea396f09e3153255f8fcf
eddiesr123 <eddiesr1234@gmail.com>
2020-05-02 02:00:06 -0700

    wired up clarifai face detection api need to add sensor boxes for detected faces

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Logo from './components/Logo/Logo';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Particles from 'react-particles-js';
+import { particlesOptions } from './apis/particles';
+import detectFaces from './apis/clarifai';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		input: '',
+		imageUrl: ''
+	};
+
+	onInputChange = (event) => {
+		this.setState({
+			input: event.target.value
+		});
+	};
+
+	onButtonSubmit = () => {
+		this.setState(
+			{
+				imageUrl: this.state.input
+			},
+			() => detectFaces(this.state.imageUrl)
+		);
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<Particles className="particles" params={particlesOptions} />
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+				<FaceRecognition imageUrl={this.state.imageUrl} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/apis/particles.js
@@ -0,0 +1,11 @@
+export const particlesOptions = {
+	particles: {
+		number: {
+			value: 200,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+};
